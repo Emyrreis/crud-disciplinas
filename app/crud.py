4110b4934fbede268fe9613da1debaf0205cbb0f
Emyrreis <vitoria.emylly0405@gmail.com>
2026-04-12 23:34:18 -0300

    criando o crud das disciplinas

---
+++ app/crud.py
@@ -0,0 +1,34 @@
+disciplines = []
+
+def create_discipline(titulo, dtinicio, dtfinal, vagas, verao):
+    if vagas < 0:
+        raise ValueError("Número de vagas inválido")
+
+    discipline = {
+        "title": titulo,
+        "start_dt": dtinicio,
+        "end_dt": dtfinal,
+        "available_slots": vagas,
+        "summer": verao
+    }
+
+    disciplines.append(discipline)
+    return discipline
+
+
+def read_disciplines():
+    return disciplines
+
+
+def update_discipline(index, new_discipline):
+    if index >= len(disciplines):
+        raise IndexError("Disciplina não encontrada")
+
+    disciplines[index].update(new_discipline)
+
+
+def deletar_disciplina(index):
+    if index >= len(disciplines):
+        raise IndexError("Disciplina não encontrada")
+
+    disciplines.pop(index)
